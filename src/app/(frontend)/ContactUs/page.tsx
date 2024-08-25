0ee7baa1ef106904e255eda24a678a2e5e11d2b3
mayankjonwal02 <jonwal.1@iitj.ac.in>
2024-08-25 20:14:46 +0530

    changes

--- src/app/(frontend)/ContactUs/page.tsx
+++ src/app/(frontend)/ContactUs/page.tsx
@@ -1,12 +1,77 @@
 import React from 'react'
-// import img1 from "../../assets/Contact_img_1.jpg"
+import img1 from "../../assets/ContactUs_img_1.jpg"
+import img2 from "../../assets/ContactUs_img_2.jpg"
+import { Input } from '@/components/ui/input'
+import { Button } from '@/components/ui/button'
 
 const ContactUs = () => {
   return (
     <div>
-      <div>
-        {/* <img src={img1.src} alt="img1" className="w-full" /> */}
+      <div className='relative w-full'>
+        <img src={img1.src} alt="img1" className="w-full" />
+        <div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-5xl font-extrabold'>Contact Us</div>
       </div>
+
+
+      <div className='w-full flex flex-row justify-evenly items-center my-[150px]'>
+
+        <div className='w-1/3 flex flex-col justify-center items-start'>
+          <div className='text-[#524FF5] font-mono  text-2xl'>WELCOME TO STRENGTHY</div>
+          <div className='text-3xl font-bold mb-6'>Get In Touch With Us</div>
+          <div>If you have any feedback or questions about our clubs, our website or our services in general, please contact us by filling out the form.</div>
+          <div className='text-xl font-semibold mb-3 mt-7'>Open Hours</div>
+          <div className='w-1/2 h-[1px] bg-[#D3D3D3] mb-3'></div>
+
+          <div className='mb-5'><b>Mon-Fri:</b>08:00 AM To 09:00 PM</div>
+          <div className='mb-5'><b>Sat:</b>09:00 AM To 06:00 PM</div>
+          <div className='mb-5'><b>Sun:</b>09:00 AM To 02:00 PM</div>
+
+
+        </div>
+
+        <div className='w-1/3 flex flex-col justify-center items-start bg-[#F9F9F9] p-[50px]'>
+          <div className='text-3xl font-bold '>Send Us a Message</div>
+          <div className='text-[#524FF5] mb-7  text-md'>Your email address will not be published *</div>
+          <Input className='w-full mb-5' type="text" placeholder="Your Full Name" />
+          <Input className='w-full mb-5' type="email" placeholder="Email Address" />
+          <Input className='w-full mb-5' type="text" placeholder="Subject" />
+          <Input className='w-full mb-5 min-h-[100px] overflow-auto resize-y' type="text" placeholder="Message" />
+
+          <Button className='w-fit mb-5 bg-black text-white'>Send Now</Button>
+        </div>
+      </div>
+
+
+      <div className='relative w-full'>
+        <img src={img2.src} alt="img2" className="w-full" />
+        <div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-black bg-[#7E6D5B] flex flex-row justify-center items-start w-3/5 h-[250px] p-6'>
+          <div className='w-1/3 flex flex-col justify-center items-center gap-5 '>
+            <div className='text-2xl font-semibold'>Phone</div>
+            <div className='w-1/2 h-[1px] bg-black'></div>
+            <div className='text-md text-center'>Capitalized on hanging fruit to identify with additional</div>
+            <div className='font-bold'>+91234567890</div>
+          </div>
+
+          <div className='w-[1px] bg-black h-full'></div>
+
+          <div className='w-1/3 flex flex-col justify-center items-center gap-5 '>
+            <div className='text-2xl font-semibold'>Mail</div>
+            <div className='w-1/2 h-[1px] bg-black'></div>
+            <div className='text-md text-center'>Capitalized on hanging fruit to identify with additional</div>
+            <div className='font-bold'>hello@strengthy.com</div>
+          </div>
+
+          <div className='w-[1px] bg-black h-full' ></div>
+
+          <div className='w-1/3 flex flex-col justify-center items-center gap-5 ='>
+            <div className='text-2xl font-semibold'>Location</div>
+            <div className='w-1/2 h-[1px] bg-black'></div>
+            <div className='text-md text-center'>Capitalized on hanging fruit to identify with additional</div>
+            <div className='font-bold'>123 King Dr.Oswego, NY 13126</div>
+          </div>
+        </div>
+      </div>
+
     </div>
   )
 }
